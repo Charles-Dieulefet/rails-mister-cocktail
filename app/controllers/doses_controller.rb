4552ef5a75be9e7393cae17947d45644c6a80960
Charles Dieulefet <charles.dieulefet@gmail.com>
2019-08-16 11:33:07 +0200

    done

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,43 @@
+class DosesController < ApplicationController
+
+  def index
+    @doses = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail_id = @cocktail.id
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
+  # def set_doses
+  #   @dose = dose.find(params[:id])
+  # end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'Faker'
+require 'open-uri'
+require 'json'
+
+  response = open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+  json = JSON.parse(response.read)
+  word = json['drinks'].map {|i| i.values}.flatten
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+150.times do
+Ingredient.create(name: word.sample)
+end
+
+Dose.create(description: 3, cocktail_id: 1, ingredient_id: 1)
+Dose.create(description: 4, cocktail_id: 2, ingredient_id: 2)
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Moscow Mule")
+
+# private
